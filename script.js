57257af9988c49755966f4e2b20221f8726503a7
github username <github email address>
2018-10-06 14:14:11 -0400

    remove audio

--- script.js
+++ script.js
@@ -189,7 +189,7 @@ document.addEventListener('DOMContentLoaded', () => {
           image.style.top = "10px"
           image.style.left = "10px"
           image.style.margin = "19px"
-          image.style.opacity = "0.9"
+          image.style.opacity = "0.8"
           image.style.borderRight = "thin solid #fff"
           image.style.borderLeft = "thin solid #fff"
         }
@@ -328,7 +328,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // {
     //     let audioBlob = xhr.response;//xhr.response is now a blob object
     // }
-    // debugger 
+    // debugger
 
     audio.style.display = "block"
     console.log('AUDIO.SRC(BLOB?): ', audio.src)
@@ -386,13 +386,6 @@ document.addEventListener('DOMContentLoaded', () => {
           g = 50 * (i/bufferLength) + 160;
           b = 255
         } else if (dataArray[i] > 180){
-<<<<<<< HEAD
-          r = barHeight + (5000 * (i/bufferLength)) + 10
-          g = 50 * (i/bufferLength) + 40
-          b = 250
-        } else if (dataArray[i] > 100){
-          r = barHeight + (500 * (i/bufferLength))
-=======
           r = 0
           g = 50 * (i/bufferLength) + 130;
           b = 255
@@ -402,7 +395,6 @@ document.addEventListener('DOMContentLoaded', () => {
           b = 255
         } else if (dataArray[i] > 130){
           r = barHeight + ((i/bufferLength))
->>>>>>> gina
           g = 50 * (i/bufferLength) + 80
           b = 255
         } else if (dataArray[i] < 90){
