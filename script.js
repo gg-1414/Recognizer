01e4cc1fdbe40288398037c8eb13da36fc8d4462
Flatiron School <flatironschool@Flatirons-MacBook-Air-5.local>
2018-10-25 16:22:59 -0400

    added songs

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const API_SECRET = "UX5x4BxXjM7XBDUwB1TEZv9IxZ7asT_J"
     const form = document.querySelector('#test_form')
 
+
      $.ajax({
        type: 'POST',
        url: API_URL,
@@ -328,7 +329,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // {
     //     let audioBlob = xhr.response;//xhr.response is now a blob object
     // }
-    // debugger 
+    // debugger
 
     audio.style.display = "block"
     console.log('AUDIO.SRC(BLOB?): ', audio.src)
@@ -386,13 +387,11 @@ document.addEventListener('DOMContentLoaded', () => {
           g = 50 * (i/bufferLength) + 160;
           b = 255
         } else if (dataArray[i] > 180){
-<<<<<<< HEAD
           r = barHeight + (5000 * (i/bufferLength)) + 10
           g = 50 * (i/bufferLength) + 40
           b = 250
         } else if (dataArray[i] > 100){
           r = barHeight + (500 * (i/bufferLength))
-=======
           r = 0
           g = 50 * (i/bufferLength) + 130;
           b = 255
@@ -402,7 +401,6 @@ document.addEventListener('DOMContentLoaded', () => {
           b = 255
         } else if (dataArray[i] > 130){
           r = barHeight + ((i/bufferLength))
->>>>>>> gina
           g = 50 * (i/bufferLength) + 80
           b = 255
         } else if (dataArray[i] < 90){
